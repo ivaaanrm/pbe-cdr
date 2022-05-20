ed136a2b835ac94949eb377ee456b8b1a5cc69ab
Nacho <67743899+isanchop@users.noreply.github.com>
2022-05-21 01:23:52 +0200

    remove logs

--- clients/web/src/js/loginPage.js
+++ clients/web/src/js/loginPage.js
@@ -7,12 +7,10 @@ async function httprequestlogin() {
     sessionStorage.setItem("id", password);
 
     res = Http.open("GET", 'http://localhost:3000/' + password + '?username=' + usuario);
-    console.log('http://localhost:3000/' + password + '?username=' + usuario)
     Http.responseType = 'json';
     Http.send();
 
     Http.onreadystatechange = (e) => {
-        console.log(Http.status)
         if(Http.readyState == XMLHttpRequest.DONE && Http.status != 200){
             alert("Incorrect username or password.")
         }
